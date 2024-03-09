09717e8b2aa43c29281ce7716b7415decc209e04
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-03-09 18:56:44 +0530

    rm to rm fix

--- src/Pages/Store/MaterialTransfer/RM-REJ/TransactionRej.jsx
+++ src/Pages/Store/MaterialTransfer/RM-REJ/TransactionRej.jsx
@@ -25,7 +25,7 @@ function TransactionRej() {
   const columns = [
     { field: "date", headerName: "Date", width: 150 },
     { field: "part", headerName: "Part Code", width: 90 },
-    { field: "cat_part", headerName: "Cat Part Code", width: 90 },
+    { field: "cat_part", headerName: "Cat Part Code", width: 120 },
     { field: "name", headerName: "Component", width: 390 },
     { field: "out_location", headerName: "Out Location", width: 150 },
     { field: "in_location", headerName: "In Location", width: 150 },
