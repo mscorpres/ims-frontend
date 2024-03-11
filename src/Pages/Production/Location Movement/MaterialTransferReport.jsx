32b2beba1b6843d4339966a2f03c1a6c8f62c566
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-03-11 12:43:26 +0530

    Cat Part

--- src/Pages/Production/Location Movement/MaterialTransferReport.jsx
+++ src/Pages/Production/Location Movement/MaterialTransferReport.jsx
@@ -47,8 +47,8 @@ export default function MaterialTransferReport({ type }) {
   const columns = [
     { headerName: "Sr. No.", field: "index", width: 80 },
     { headerName: "Date", field: "date", flex: 1 },
-    { headerName: "Part Code", field: "part", width: 120 },
-    { headerName: "Cat Part Code", field: "cat_partpart", width: 120 },
+    { headerName: "Part Code", field: "part", width: 80 },
+    { headerName: "Cat Part Code", field: "cat_part", width: 120 },
     {
       headerName: "Component",
       field: "name",
