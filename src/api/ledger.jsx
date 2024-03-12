b44a3f96ecbda9f3a125550dd276e4747ca04ff6
devesh801015 <132462423+devesh801015@users.noreply.github.com>
2024-03-12 14:50:14 +0530

    Commit 1 : vendor reco

--- src/api/ledger.jsx
+++ src/api/ledger.jsx
@@ -27,3 +27,61 @@ export const getLedgerOptions = async (search) => {
   response.data.data = arr;
   return response.data;
 };
+
+export const getLedgerEmail = async (ledgerCode) => {
+  const response = await imsAxios.get(
+    `/vendorReconciliation/vendor/email?vendor=${ledgerCode}`
+  );
+  return response;
+};
+
+export const updateLedgerEmail = async (email, ledgerCode) => {
+  const payload = {
+    vendor: ledgerCode,
+    email: email,
+  };
+  const response = await imsAxios.put(
+    "/vendorReconciliation/vendor/email",
+    payload
+  );
+  return response;
+};
+
+export const sendRequestLedgerMail = async (values) => {
+  const payload = {
+    reqDate: values.date,
+    reqVendor: values.vendor.value,
+    mailFrom: values.senderEmail,
+    mailTo: values.receiverEmail,
+    subject: values.subject,
+    body: values.body,
+  };
+  const response = await imsAxios.post("/vendorReconciliation/mail", payload);
+
+  return response;
+};
+
+export const getRequestedLedgerMails = async (vendorCode) => {
+  const response = await imsAxios.get(
+    `/vendorReconciliation/mails?vendor=${vendorCode}`
+  );
+  let arr = [];
+  if (response.success) {
+    arr = response.data.map((row, index) => ({
+      id: index + 1,
+      senderEmail: row.mailFrom,
+      receiverEmail: row.mailTo,
+      requestedDate: row.requestedDate,
+      subject: row.subject,
+      status: row.status,
+      refId: row.refID,
+      body: row.body,
+      sentDate: row.sentDate,
+      attachments: row.attachments,
+      uploadedLedgers: row.uploadedLedgers,
+    }));
+    response.data = arr;
+  }
+
+  return response;
+};
