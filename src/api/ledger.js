dc9a8948002e20a1e659b877768d9e10ee107e68
devesh801015 <132462423+devesh801015@users.noreply.github.com>
2024-03-15 11:57:03 +0530

    changed api and utils file extension from jsx to js

--- src/api/ledger.js
+++ src/api/ledger.js
@@ -1,4 +1,5 @@
 import { imsAxios } from "../axiosInterceptor";
+import { convertSelectOptions } from "../utils/general";
 
 export const getLedgerReport = async (payload) => {
   try {
@@ -11,4 +12,76 @@ export const getLedgerReport = async (payload) => {
   } catch (error) {
     console.log("something happened wrong", error);
   }
+};
+
+export const getLedgerOptions = async (search) => {
+  const response = await imsAxios.post("/tally/ledger/ledger_options", {
+    search,
+  });
+
+  let arr = [];
+  if (response.data.code === 200) {
+    arr = convertSelectOptions(response.data.data);
+  }
+
+  response.data.data = arr;
+  return response.data;
+};
+
+export const getLedgerEmail = async (ledgerCode) => {
+  const response = await imsAxios.get(
+    `/vendorReconciliation/vendor/email?vendor=${ledgerCode}`
+  );
+  return response;
+};
+
+export const updateLedgerEmail = async (email, ledgerCode) => {
+  const payload = {
+    vendor: ledgerCode,
+    email: email,
+  };
+  const response = await imsAxios.put(
+    "/vendorReconciliation/vendor/email",
+    payload
+  );
+  return response;
+};
+
+export const sendRequestLedgerMail = async (values) => {
+  const payload = {
+    reqDate: values.date,
+    reqVendor: values.vendor.value,
+    mailFrom: values.senderEmail,
+    mailTo: values.receiverEmail,
+    subject: values.subject,
+    body: values.body,
+  };
+  const response = await imsAxios.post("/vendorReconciliation/mail", payload);
+
+  return response;
+};
+
+export const getRequestedLedgerMails = async (vendorCode) => {
+  const response = await imsAxios.get(
+    `/vendorReconciliation/mails?vendor=${vendorCode}`
+  );
+  let arr = [];
+  if (response.success) {
+    arr = response.data.map((row, index) => ({
+      id: index + 1,
+      senderEmail: row.mailFrom,
+      receiverEmail: row.mailTo,
+      requestedDate: row.requestedDate,
+      subject: row.subject,
+      status: row.status,
+      refId: row.refID,
+      body: row.body,
+      sentDate: row.sentDate,
+      attachments: row.attachments,
+      uploadedLedgers: row.uploadedLedgers,
+    }));
+    response.data = arr;
+  }
+
+  return response;
 };
--- src/api/sales/salesOrder.js
+++ src/api/sales/salesOrder.js
@@ -35,6 +35,48 @@ export const cancelTheSelectedSo = async (values) => {
   });
   return response;
 };
+export const listOfShipment = async (searchTerm, wise) => {
+  const response = await imsAxios.post(
+    "/so_challan_shipment/fetchSalesOrderShipmentlist",
+    {
+      wise: wise,
+      data: searchTerm,
+    }
+  );
+  return response;
+};
+export const getUpdateShipmentDetails = async (searchTerm) => {
+  const response = await imsAxios.post(
+    "/so_challan_shipment/fetchShipmentforUpdate",
+    {
+      shipment_id: searchTerm,
+    }
+  );
+  return response;
+};
+export const getChallanList = async (wise, searchTerm) => {
+  const response = await imsAxios.post(
+    "/so_challan_shipment/fetchDeliveryChallan",
+    {
+      data: searchTerm,
+      wise: wise,
+    }
+  );
+  let arr = [];
+  if (response.success) {
+    arr = response.data.map((row, index) => ({
+      id: index + 1,
+      date: row.delivery_challan_dt,
+      challanId: row.so_challan_id,
+      clientCode: row.client_code,
+      client: row.client,
+      billingAddress: row.billingaddress,
+      shippingAddress: row.shippingaddress,
+    }));
+  }
+  response.data = arr;
+  return response;
+};
 
 export const printOrder = async (orderId) => {
   const payload = {
@@ -44,12 +86,43 @@ export const printOrder = async (orderId) => {
   return response;
 };
 
-export const createShipment = async (values) => {
+export const createChallanFromSo = async (shipments, remark) => {
+  let payload = {
+    shipment_id: shipments.map((r) => r.shipmentId),
+    so_id: shipments.map((r) => r.orderId),
+    client_id: shipments[0].clientCode,
+    client_addr_id: shipments[0].clientAddressId,
+    bill_id: shipments[0].billingId,
+    ship_id: shipments[0].shippingId,
+    remark: remark,
+  };
+  const response = await imsAxios.post(
+    "/so_challan_shipment/createDeliveryChallan",
+    payload
+  );
+  return response;
+};
+export const cancelChallanFromSo = async (singleRow, remark) => {
+  console.log("singlerwo", singleRow);
+  let payload = {
+    so_shipment_id: singleRow.shipment_id,
+    remark: remark,
+  };
+
+  console.log("payload", remark);
+  const response = await imsAxios.post(
+    "/so_challan_shipment/cancelSOshipment",
+    payload
+  );
+  return response;
+};
+
+export const createShipment = async (values, open) => {
   const payload = {
     header: {
       bill_id: values.billingId,
       bill_addr: values.billingAddress,
-      so_id: "SO/23-24/0022",
+      so_id: open,
       ship_id: values.shippingId,
       ship_addr: values.shippingAddress,
       vehicle_no: values.vehicleNo,
@@ -65,5 +138,109 @@ export const createShipment = async (values) => {
       remark: values.products.map((row) => row.remark),
     },
   };
-  console.log("this is the values", payload);
+  const response = await imsAxios.post(
+    "/so_challan_shipment/saveSOShipment",
+    payload
+  );
+  return response;
+  // console.log("this is the values", payload);
+};
+
+export const fetchShipmentDetails = async (shipmentId) => {
+  const payload = {
+    so_shipment_id: shipmentId,
+  };
+  const response = await imsAxios.post(
+    "/so_challan_shipment/fetchSOShipmentDetails",
+    payload
+  );
+  let arr = [];
+  if (response.success) {
+    arr = response.data.map((row, index) => ({
+      id: index + 1,
+      partCode: row.item_code,
+      component: row.item_name,
+      hsn: row.hsn,
+      pickLocation: row.item_pick_location_name,
+      pickLocationKey: row.item_pick_location,
+      rate: row.price,
+      qty: row.qty,
+      remark: row.remark,
+    }));
+  }
+  response.data = arr;
+  return response;
+};
+
+export const getChallanDetails = async (challanId) => {
+  const payload = {
+    challan_id: challanId,
+  };
+  const response = await imsAxios.post(
+    "/so_challan_shipment/getDeliveryChallanDetails",
+    payload
+  );
+  let arr = [];
+  let obj = {};
+  if (response.success) {
+    const first = response.data[0];
+    arr = response.data.map((row, index) => ({
+      id: index + 1,
+      orderId: row.so_id,
+      shipmentId: row.shipment_id,
+      partCode: row.item_part_no,
+      component: row.item_name,
+      componentKey: row.item_id,
+      rate: row.item_rate,
+      qty: row.item_qty,
+      remark: row.remark,
+    }));
+
+    obj = {
+      clientCode: first.client_code,
+      client: first.client,
+      clientAddress: first.clientaddress,
+      billingAddress: first.billingaddress,
+      shippingAddress: first.shippingaddress,
+    };
+  }
+  response.data = { components: arr, details: obj };
+  return response;
+};
+export const updateShipment = async (values, open, updateShipmentRow) => {
+  let billId;
+  if (values?.billingId?.id) {
+    billId = values?.billingId?.id;
+  } else {
+    billId = values?.billingId;
+  }
+  const payload = {
+    headers: {
+      bill_id: billId,
+      bill_addr: values.billingAddress,
+      so_shipment_id: open,
+      ship_id: values.shippingId,
+      ship_addr: values.shippingAddress,
+      vehicle_no: values.vehicleNo,
+      eway_no: values.eWayBillNo,
+      other_ref: values.otherRef,
+      so_id: updateShipmentRow?.so_id,
+    },
+    materials: {
+      updaterow: values.products.map((row) => row.updaterow.toString()),
+      item: values.products.map((row) => row.productKey),
+      qty: values.products.map((row) => row.qty),
+      rate: values.products.map((row) => row.rate),
+      // picklocation: values.products.map((row) => row.pickLocation),
+      hsn: values.products.map((row) => row.hsn),
+      remark: values.products.map((row) => row.remark),
+    },
+  };
+  // // return;
+  const response = await imsAxios.post(
+    "/so_challan_shipment/updateSOshipment",
+    payload
+  );
+  return response;
+  // console.log("this is the values", payload);
 };
