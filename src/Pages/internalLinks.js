a7ff1d8d66fcc81f397f45b986ec3acd404e1666
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-03-20 12:28:24 +0530

    32 link added

--- src/Pages/internalLinks.js
+++ src/Pages/internalLinks.js
@@ -1,6 +1,7 @@
 import routeConstants from "../Routes/routeConstants";
 
 const links = [
+  // purchase order links
   [
     {
       routeName: "Home",
@@ -13,7 +14,6 @@ const links = [
       key: 0,
     },
   ],
-  // purchase order links
   [
     {
       routeName: "Create PO",
@@ -764,6 +764,13 @@ const links = [
 
       placeholder: "Vendor RM Consumption Report",
     },
+
+    {
+      routeName: "R32",
+      routePath: "/r32",
+
+      placeholder: "Cost Center Transaction Report",
+    },
   ],
   // MIN label links
 
@@ -1100,14 +1107,14 @@ const links = [
     },
   ],
   [
-    {
-      routeName: "All Pages",
-      routePath: "/controlPanel/allPages",
-    },
-    {
-      routeName: "All Users",
-      routePath: "/controlPanel/allUsers",
-    },
+    // {
+    //   routeName: "All Pages",
+    //   routePath: "/controlPanel/allPages",
+    // },
+    // {
+    //   routeName: "All Users",
+    //   routePath: "/controlPanel/allUsers",
+    // },
     ///
     {
       routeName: "Registered Users",
