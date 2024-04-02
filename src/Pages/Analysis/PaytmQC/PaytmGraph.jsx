f4f3fefe4e0596eacd5dfe8adfd5812c01fc0b05
devesh801015 <132462423+devesh801015@users.noreply.github.com>
2024-04-02 10:32:02 +0530

    removed some not used ant imports

--- src/Pages/Analysis/PaytmQC/PaytmGraph.jsx
+++ src/Pages/Analysis/PaytmQC/PaytmGraph.jsx
@@ -1,8 +1,5 @@
 import { Card, Col, Divider, Drawer, Row, Typography } from "antd";
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import { Column } from "@ant-design/plots";
 import { CChart } from "@coreui/react-chartjs";
 
 function PaytmGraph({
