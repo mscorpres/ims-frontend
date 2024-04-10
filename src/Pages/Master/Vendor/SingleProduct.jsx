22f955f45b91682b72febd9408f4a9d36b12bca0
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-04-10 17:10:01 +0530

    multiple uplaod

---
+++ src/Pages/Master/Vendor/SingleProduct.jsx
@@ -0,0 +1,77 @@
+import { Button, Col, Form, Input, Row } from "antd";
+import UploadDocs from "../../Store/MaterialIn/MaterialInWithPO/UploadDocs";
+
+export default function SingleProduct({
+  index,
+  field,
+  fields,
+  add,
+  remove,
+  form,
+  setFiles,
+  files,
+  // allTdsOptions,
+  // tdsArray,
+  // optionState,
+  // setOptionState,
+}) {
+  const component =
+    Form.useWatch(["components", field.name, "component"], form) ?? 0;
+  console.log("here", form);
+
+  return (
+    <Row
+      style={{
+        // background: "#f5f5f58f",
+        padding: "5px 15px",
+        borderRadius: 10,
+        marginTop: 5,
+      }}
+      gutter={[6, 10]}
+      key={field.key}
+    >
+      <Col span={6}>
+        {" "}
+        <Form.Item label="Document Name" name={[field.name, "documentName"]}>
+          <Input />
+        </Form.Item>
+      </Col>
+      <Col span={6}>
+        <Form.Item label="File" name={[field.name, "file"]}>
+          <UploadDocs
+            type="secondary"
+            size="large"
+            // disable={poData?.materials?.length == 0}
+            setFiles={setFiles}
+            names={"names"}
+            files={files}
+          />
+        </Form.Item>
+      </Col>
+      <Col span={12}>
+        <Row justify="end" align="middle" style={{ height: "100%" }}>
+          {fields.length > 1 && (
+            <Button
+              onClick={() => remove(field.name)}
+              danger
+              type="text"
+              size="small"
+            >
+              - Remove Component
+            </Button>
+          )}
+          <Button size="small" type="link" onClick={() => add()}>
+            + Add Component
+          </Button>
+        </Row>
+      </Col>
+    </Row>
+  );
+}
+const gstRateOptions = [
+  { value: "0", text: "00%" },
+  { value: "5", text: "05%" },
+  { value: "12", text: "12%" },
+  { value: "18", text: "18%" },
+  { value: "28", text: "28%" },
+];
--- src/Pages/Store/MaterialIn/MaterialInWithPO/UploadDocs.jsx
+++ src/Pages/Store/MaterialIn/MaterialInWithPO/UploadDocs.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import { Button, Drawer, Upload } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 
-export default function UploadDocs({ files, setFiles, disable, size }) {
+export default function UploadDocs({
+  files,
+  setFiles,
+  disable,
+  size,
+  names,
+  type,
+}) {
+  console.log("files, setFiles, disable, size ", files, names);
   const props = {
-    maxCount: 1,
+    maxCount: 2,
+    name: "name",
     showUploadList: files.length > 0,
     onRemove: (file) => {
       const index = files.indexOf(file);
@@ -20,16 +29,18 @@ export default function UploadDocs({ files, setFiles, disable, size }) {
   };
 
   return (
-    <Upload {...props}>
-      <Button
-        disabled={disable}
-        type="primary"
-        size={size && "large"}
-        style={{ marginRight: size === "large" && 20 }}
-        icon={<UploadOutlined />}
-      >
-        Upload File
-      </Button>
-    </Upload>
+    <>
+      <Upload {...props}>
+        <Button
+          disabled={disable}
+          type={type && "primary"}
+          size={size && "large"}
+          style={{ marginRight: size === "large" && 20 }}
+          icon={<UploadOutlined />}
+        >
+          Upload File
+        </Button>
+      </Upload>
+    </>
   );
 }
