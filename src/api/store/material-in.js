d40feacb7ddc805edffabef37bcb74b88ba2ac40
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-04-22 18:00:18 +0530

    multiple uplaod pt 1

--- src/api/store/material-in.js
+++ src/api/store/material-in.js
@@ -17,9 +17,9 @@ export const validateInvoiceforSFG = async (values) => {
   return response;
 };
 
-export const uploadMinInvoice = async (file) => {
-  const formData = new FormData();
-  formData.append("files", file);
+export const uploadMinInvoice = async (formData) => {
+  // const formData = new FormData();
+  // formData.append("files", file);
   const response = await imsAxios.post("/transaction/upload-invoice", formData);
 
   return response;
@@ -38,7 +38,8 @@ export const materialInWithoutPo = async (values, fileName) => {
     irn: values.irn,
     qrScan: values.QR == true ? "Y" : "N",
 
-    component: values.components.map((row) => row.component.value),
+    documentName: values.fileComponents?.map((r) => r.documentName),
+    component: values?.components.map((row) => row.component?.value),
     qty: values.components.map((row) => row.qty),
     rate: values.components.map((row) => row.rate),
     currency: values.components.map((row) => row.currency),
@@ -55,7 +56,8 @@ export const materialInWithoutPo = async (values, fileName) => {
     location: values.components.map((row) => row.location.value),
     out_location: values.components.map((row) => row.autoConsumption),
   };
-
+  // console.log("payload",payload)
+  // return
   const response = await imsAxios.post("/transaction/min_transaction", payload);
 
   return response;
