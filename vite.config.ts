d4d31054dd0bbe7582b410ef99f73c6a7ef0bcea
mscorpres <iot@mscorpres.in>
2024-05-03 09:59:56 +0530

    removed cahcedCheks in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    fs: {
-      cachedChecks: false,
-    },
+    // fs: {
+    //   cachedChecks: false,
+    // },
   },
 });
