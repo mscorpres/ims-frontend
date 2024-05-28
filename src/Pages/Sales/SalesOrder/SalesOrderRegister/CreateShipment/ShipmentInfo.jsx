bb701f989f9d9ede07cf6187c8a6bb89ccff2c25
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-05-28 13:36:58 +0530

    laoding in create shipemt

--- src/Pages/Sales/SalesOrder/SalesOrderRegister/CreateShipment/ShipmentInfo.jsx
+++ src/Pages/Sales/SalesOrder/SalesOrderRegister/CreateShipment/ShipmentInfo.jsx
@@ -67,6 +67,7 @@ const ShipmentInfo = ({
             onClick={validateHandler}
             variant="submit"
             text={updateShipmentRow ? "Update Shipment" : "Create Shipment"}
+            loading={"select"}
           />
         </Row>
       </Card>
