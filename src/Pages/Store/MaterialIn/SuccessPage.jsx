d0a14a7e8d33d72120981bdcb95a4c8732585c2c
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-09-18 16:44:27 +0530

    print removed  temp-asked by shiv sir

--- src/Pages/Store/MaterialIn/SuccessPage.jsx
+++ src/Pages/Store/MaterialIn/SuccessPage.jsx
@@ -72,13 +72,13 @@ export default function SuccessPage({
             <Col>
               <CommonIcons action={"refreshButton"} onClick={newMinFunction} />
             </Col>
-            <Col>
+            {/* <Col>
               <CommonIcons
                 loading={printLoading}
                 action={"printButton"}
                 onClick={printFun}
               />
-            </Col>
+            </Col> */}
             <Col>
               <CommonIcons
                 loading={downloadLoading}
