573a9b04e238253d8b6d6bda5a3ee83fc58181fb
sanchitvarshney <sanchit.varshney@mscorpres.in>
2024-12-11 16:14:19 +0530

    Merge branch 'dev' of https://github.com/mscorpres/ims-frontend into dev

--- src/Pages/Jobwork/Modal/JwReturnModel/Components.jsx
+++ src/Pages/Jobwork/Modal/JwReturnModel/Components.jsx
@@ -69,6 +69,7 @@ const Components = ({
               <MyButton
                 variant="add"
                 disabled={selectedRows.length === 0}
+                // disabled={selectedRows.length === 0}
                 onClick={addComponent}
                 style={{ marginLeft: 10 }}
               />
