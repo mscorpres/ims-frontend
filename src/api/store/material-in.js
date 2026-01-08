acd0131fb5c0e3a61c08eb9b0ea069f346b81989
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-01-08 12:55:48 +0530

    fixed issue

--- src/api/store/material-in.js
+++ src/api/store/material-in.js
@@ -90,8 +90,7 @@ export const materialInWithoutPo = async (values, fileName, vendorType) => {
     location: values.components.map((row) => row.location.value),
     out_location: values.components.map((row) => row.autoConsumption),
   };
-  console.log("payload",payload)
-  return
+
   const response = await imsAxios.post("/transaction/min_transaction", payload);
 
   return response;
