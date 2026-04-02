fabc415dfa7ef2948815c87c5f95ee4e8b391918
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-04-02 16:10:07 +0530

    fix login code

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -92,7 +92,7 @@ const Login = () => {
           // Two-step login, show OTP screen
           setUserCredentials({
             username,
-            token: res?.data?.tempToken,
+            token: res?.data?.token,
             qrCode: res?.data?.qrCode,
             company_branch: inpVal.company_branch, // Store selected branch for OTP flow
           });
@@ -102,7 +102,7 @@ const Login = () => {
         } else {
           // Normal login flow (no OTP)
           const payload = res?.data ?? res;
-       
+        
           const obj = {
             email: payload.crn_email,
             phone: payload.crn_mobile,
@@ -398,7 +398,7 @@ const Login = () => {
 
       if (response?.success) {
         const payload = response?.data;
-    
+        console.log(payload, "data");
       
 
         const obj = {
