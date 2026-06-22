1af8382e6ad13004b0f8cbe03e87cfc936817f4b
MsCorpres Automation <iot@mscorpres.in>
2026-06-22 12:28:00 +0530

    3-Add Modifications in Rm partcode conversion

--- src/Pages/Store/RMPartCodeConversion/index.jsx
+++ src/Pages/Store/RMPartCodeConversion/index.jsx
@@ -267,7 +267,7 @@ const RMPartCodeConversion = () => {
         component_out: addedComponents.out.component.value,
         qty_out: addedComponents.out.qty,
         loc_out: addedComponents.out.location.value,
-        rate: [componentRates[addedComponents.out.component.value] ?? 0],
+        rate: [componentRates[addedComponents.in[0].component.value] ?? 0],// rate of initial component is used for final component
       },
     };
     Modal.confirm({
